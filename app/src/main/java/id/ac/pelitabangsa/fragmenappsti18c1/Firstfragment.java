211838bfa478ce0a80f004ccc76d37acf74d4924
ericofernandy <ericofernandy12@gmail.com>
2021-04-11 20:29:52 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/pelitabangsa/fragmenappsti18c1/Firstfragment.java
@@ -0,0 +1,6 @@
+package id.ac.pelitabangsa.fragmenappsti18c1;
+
+import android.app.Fragment;
+
+public class Firstfragment extends Fragment {
+}
---
+++ app/src/main/java/id/ac/pelitabangsa/fragmenappsti18c1/SecondActivity.java
@@ -0,0 +1,6 @@
+package id.ac.pelitabangsa.fragmenappsti18c1;
+
+import android.app.Activity;
+
+public class SecondActivity extends Activity {
+}
